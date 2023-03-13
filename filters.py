888d6754ebff4b8c9cec9ac41192bd8e28a9c32d
MountassirEl <mountassir.elmoustaaid@akanthas.com>
2023-03-13 20:20:52 +0100

    save: initiale state of the projet

---
+++ filters.py
@@ -0,0 +1,304 @@
+"""
+This file contains the DAN and function to construct the neural networks
+"""
+import torch
+from torch import nn
+from torch.distributions.multivariate_normal import MultivariateNormal as Mvn
+import numpy as np 
+
+class DAN(nn.Module):
+    """
+    A Data Assimilation Network class
+    """
+    def __init__(self, a_kwargs, b_kwargs, c_kwargs):
+
+        nn.Module.__init__(self)
+        self.a = Constructor(**a_kwargs)
+        self.b = Constructor(**b_kwargs)
+        self.c = Constructor(**c_kwargs)
+        self.scores = {
+            "RMSE_b": [],
+            "RMSE_a": [],
+            "LOGPDF_b": [],
+            "LOGPDF_a": [],
+            "LOSS": []}
+
+    def forward(self, ha, x, y):
+        """
+        forward pass in the DAN
+        """
+
+        # TODO
+        # propagate past mem into prior mem
+        # translate prior mem into prior pdf        
+        # analyze prior mem
+        # translate post mem into post pdf
+        logpdf_a = None
+        
+        # TODO rewrite loss 
+        loss = 0
+        
+        # Compute scores
+        with torch.no_grad():
+            if logpdf_a is not None:
+                self.scores["RMSE_b"].append(torch.mean(torch.norm(
+                    pdf_b.mean - x, dim=1)*x.size(1)**-.5).item())
+                self.scores["RMSE_a"].append(torch.mean(torch.norm(
+                    pdf_a.mean - x, dim=1)*x.size(1)**-.5).item())
+                self.scores["LOGPDF_b"].append(logpdf_b.item())
+                self.scores["LOGPDF_a"].append(logpdf_a.item())
+                self.scores["LOSS"].append(loss.item())
+                
+        return loss, ha
+
+    def clear_scores(self):
+        """ clear the score lists
+        """
+        for v in self.scores.values():
+            v.clear()
+
+class Id(nn.Module):
+    """ A simple id function
+    """
+    def __init__(self):
+        nn.Module.__init__(self)
+
+    def forward(self, x):
+        """ trivial
+        """
+        return x
+
+class Cst(nn.Module):
+    """ A constant scale_vec
+    """
+    def __init__(self, init, dim=None):
+        nn.Module.__init__(self)
+        if isinstance(init, torch.Tensor):
+            self.c = init.unsqueeze(0)
+        else:
+            raise NameError("Cst init unknown")
+
+    def forward(self, x):
+        return self.c.expand(x.size(0), self.c.size(0))
+
+class Lin2d(nn.Module):
+    # rotation dymnamics
+    def __init__(self, x_dim, N, dt, init,
+                 window=None):
+        assert(x_dim == 2)
+        nn.Module.__init__(self)
+        # TODO
+        # implement M
+        
+    def forward(self, x):
+        # input x: (mb,x_dim)
+        # output Mx: (mb,x_dim)
+        # TODO
+        Mx = torch.zeros(x.size())
+        return Mx
+    
+class EDO(nn.Module):
+    """ Integrates an EDO with RK4
+    """
+    def __init__(self, x_dim, N, dt, init,
+                 window=None):
+        nn.Module.__init__(self)
+        self.x_dim = x_dim
+        self.N = N
+        self.dt = dt
+        if init == "95":
+            """ Lorenz95 (96) initialization
+            """
+            self.window = (-2, -1, 0, 1)
+            self.diameter = 4
+            self.A = torch.tensor([[[0., 0., 0., 0.],
+                                  [-1., 0., 0., 0.],
+                                  [0., 0., 0., 0.],
+                                  [0., 1., 0., 0.]]])
+            self.b = torch.tensor([[0., 0., -1., 0.]])
+            self.c = torch.tensor([8.])
+        else:
+            raise NameError("EDO init not available")
+
+    def edo(self, x):
+        # input x: (mb,x_dim)
+        # output dx/dt: (mb,x_dim)
+        # Hint: convert x into v (mb,x_dim,4), then reshape into (mb*x_dim,4)
+        # and apply the matrix self.A using torch.nn.functional.bilinear, etc
+        """v=
+        x-2 x-1 x0 x1
+        |   |   |  |
+        """
+        # TODO
+        dx = torch.zeros(x.size())        
+        return x
+
+    def forward(self, x):
+        for _ in range(self.N):
+            k1 = self.edo(x)
+            k2 = self.edo(x + 0.5*self.dt*k1)
+            k3 = self.edo(x + 0.5*self.dt*k2)
+            k4 = self.edo(x + self.dt*k3)
+            x = x + (self.dt/6.0)*(k1 + 2.0*k2 + 2.0*k3 + k4)
+        return x
+
+
+class FullyConnected(nn.Module):
+    """ Fully connected NN ending with a linear layer
+    """
+    def __init__(self, layers, activation_classname):
+        nn.Module.__init__(self)
+        n = len(layers)
+        self.lins = nn.ModuleList(
+            [nn.Linear(d0, d1) for
+             d0, d1 in zip(layers[:-1], layers[1:])])
+        self.acts = nn.ModuleList(
+            [eval(activation_classname)() for _ in range(n-2)])
+
+    def forward(self, h):
+        for lin, act in zip(self.lins[:-1], self.acts):
+            h = act(lin(h))
+        return self.lins[-1](h)
+
+class FcZero(nn.Module):
+    """
+    Fully connected neural network with ReZero trick
+    """
+    def __init__(self, dim, deep, activation_classname):
+        """
+        layers: the list of the layers dimensions
+        """
+        # TODO correct an error        
+        nn.Module.__init__(self)
+        layers = (deep+1)*[dim]
+        self.lins = nn.ModuleList(
+            [nn.Linear(d0, d1) for
+             d0, d1 in zip(layers[:-1], layers[1:])])
+        self.acts = nn.ModuleList(
+            [eval(activation_classname)() for _ in range(deep)])
+        self.alphas = torch.zeros(deep)
+
+    def forward(self, h):
+        for lin, act, alpha in zip(self.lins, self.acts, self.alphas):
+            h = h + alpha*act(lin(h))
+        return h        
+
+class FcZeroLin(nn.Module):
+    """
+    FcZero network ending with linear layer
+    """
+    def __init__(self, in_dim, out_dim, deep, activation_classname):
+        """
+        layers: the list of the layers dimensions
+        """
+        nn.Module.__init__(self)
+        self.fcZero = FcZero(in_dim, deep, activation_classname)
+        self.out_dim = out_dim
+        assert(out_dim <= in_dim)
+        self.lin = FullyConnected([in_dim, out_dim], activation_classname)
+
+    def forward(self, h):
+        h = self.fcZero(h)
+        h = self.lin(h)
+        return h
+    
+
+class Gaussian(Mvn):
+    """
+    Return a pytorch Gaussian pdf from args
+    args is either a (loc, scale_tril) or a (x_dim, vec)
+    """
+    def __init__(self, *args):
+        self.minexp = torch.Tensor([-8.0])
+        self.maxexp = torch.Tensor([8.0])
+        if isinstance(args[0], int):
+            """args is a (x_dim, vec)
+            loc is the first x_dim coeff of vec
+            if the rest is one coeff c then
+                scale_tril = e^c*I
+            else
+                scale_tril is filled diagonal by diagonal
+                starting by the main one
+                (which is exponentiated to ensure strict positivity)
+            """
+            x_dim, vec = args
+            vec_dim = vec.size(-1)
+            if vec_dim == x_dim + 1:
+                #print('Mvn: scale_tril = e^c*I')
+                loc = vec[:, :x_dim]
+                scale_tril = torch.eye(x_dim)\
+                                  .unsqueeze(0)\
+                                  .expand(vec.size(0), -1, -1)
+                scale_tril = torch.exp(vec[:, x_dim])\
+                                  .view(vec.size(0), 1, 1)*scale_tril
+            else:
+                print('Mvn by mean and cov')
+                # TODO rewrite loc and scale_tril
+                # hint: use vec_to_inds
+                # vec.size(0) is the mini-batch size
+                mb = vec.size(0)
+                loc = torch.zeros(mb, x_dim)
+                scale_tril = torch.zeros(mb, x_dim, x_dim)
+                for i in range(mb):
+                    scale_tril[i,:,:] = torch.eye(x_dim)
+                
+            Mvn.__init__(self, loc=loc, scale_tril=scale_tril)
+        
+        else:
+            """args is a loc, scale_tril
+            """
+            print('Init Mvn by full arg')
+            Mvn.__init__(self, loc=args[0], scale_tril=args[1])
+
+    def vec_to_inds(self, x_dim, vec_dim):
+        """Computes the indices of scale_tril coeffs,
+        scale_tril is filled main diagonal first
+
+        x_dim: dimension of the random variable
+        vec_dim: dimension of the vector containing
+                 the coeffs of loc and scale_tril
+        """
+        ldiag, d, c = x_dim, 0, 0  # diag length, diag index, column index
+        inds = [[], []]  # list of line and column indexes
+        for i in range(vec_dim - x_dim):  # loop over the non-mean coeff
+            inds[0].append(c+d)  # line index
+            inds[1].append(c)  # column index
+            if c == ldiag-1:  # the current diag end is reached
+                ldiag += -1  # the diag length is decremented
+                c = 0  # the column index is reinitialized
+                d += 1  # the diag index is incremented
+            else:  # otherwize, only the column index is incremented
+                c += 1
+        return inds
+
+
+class Constructor(nn.Module):
+    """Construct functions and conditional Gaussians from strings and kwargs
+    - scale_vec_class is not None: return a Gaussian made from a vector,
+        this vector is made of the concatenation of loc and scale_vec
+    - scale_vec_class is None:
+        if gauss_dim is not None: return a Gaussian made from a vector,
+        else: return a vector
+    """
+    def __init__(self, loc_classname, loc_kwargs,
+                 gauss_dim=None,
+                 scale_vec_classname=None, scale_vec_kwargs=None):
+        nn.Module.__init__(self)
+        self.gauss_dim = gauss_dim
+        self.loc = eval(loc_classname)(**loc_kwargs)
+        if scale_vec_classname is not None:
+            self.scale_vec =\
+                eval(scale_vec_classname)(**scale_vec_kwargs)
+        else:
+            self.scale_vec = None
+
+    def forward(self, *args):
+        lc = self.loc(*args)
+        if self.gauss_dim is not None:
+            if self.scale_vec is not None:
+                sc = self.scale_vec(*args)
+                return Gaussian(self.gauss_dim, torch.cat((lc, sc), dim=1))
+            else:
+                return Gaussian(self.gauss_dim, lc)
+        else:
+            return lc
---
+++ manage_exp.py
@@ -0,0 +1,299 @@
+from os import mkdir, path
+import torch
+import sys
+import filters
+import os
+
+_v0 = None 
+_x0 = None
+
+def get_x0(b_size, x_dim, sigma):
+    global _x0
+    if _x0 is None:
+        _x0 = 3*torch.ones(b_size, x_dim)\
+             + sigma * torch.randn(b_size, x_dim)
+    x0 = _x0
+    return x0
+
+def get_x0_test(b_size, x_dim, sigma):
+    _x0_test = 3*torch.ones(b_size, x_dim)\
+               + sigma * torch.randn(b_size, x_dim)
+    x0 = _x0_test
+    return x0
+
+def get_ha0(b_size, h_dim):
+    global _v0
+    if _v0 is None:
+        _v0 = torch.zeros(1,h_dim)
+    ha0 = torch.zeros(b_size, h_dim)
+    for b in range(b_size):
+        ha0[b,:] = _v0
+    return ha0
+
+def set_tensor_type(tensor_type,cuda):
+    print('use gpu',cuda)
+    print('use tensor_type',tensor_type)
+    if (tensor_type == "double") and cuda:
+        torch.set_default_tensor_type(torch.cuda.DoubleTensor)
+    elif (tensor_type == "float") and cuda:
+        torch.set_default_tensor_type(torch.cuda.FloatTensor)
+    elif (tensor_type == "double") and (not cuda):
+        torch.set_default_tensor_type(torch.DoubleTensor)
+    elif (tensor_type == "float") and (not cuda):
+        torch.set_default_tensor_type(torch.FloatTensor)
+    else:
+        raise NameError("Unknown tensor_type")
+    
+def pre_train_full(net,b_size,h_dim,x_dim,
+                   sigma0,optimizer_classname,optimizer_kwargs):
+    """
+    Pre-train c at t=0
+    # learn the parameters in net.c using ha0 and x0
+    # by minimizing the L_0(q_0^a) loss
+    """
+    
+    print('Pre-train c at t=0')
+    
+    # TODO Generate x0 of batch size b_size at t=0
+    # print out the emprical mean of x0
+    
+    # create an optimizer optimizer0 for the paramerters in c
+    optimizer0 = eval(optimizer_classname)(net.c.parameters(), **optimizer_kwargs)
+    
+    # TODO minimize L_0(q_0^a), check how small is the loss
+
+    ite = 0
+    
+    # Initlize h0
+    ha0 = get_ha0(b_size, h_dim)
+    
+    # Use closure0 to compute the loss and gradients
+    def closure0():
+        # TODO first use optimizer0 to set all the gradients to zero
+        # then compute the loss logpdf_a0 = L_0(q_0^a), by using x0, h0, and c
+        # perform back-propogation of the loss
+        # return the loss logpdf_a0
+        logpdf_a0 = 0
+        
+        # a counter of number of evaluations
+        nonlocal ite
+        ite = ite + 1
+        
+        return logpdf_a0
+
+    # TODO run optimizer
+    
+    # print out the final mean and covariance of q_0^a
+    pdf_a0 = net.c(ha0)
+    print('## INIT a0 mean', pdf_a0.mean[0,:])  # first sample
+    print('## INIT a0 var', pdf_a0.variance[0,:])  # first sample
+    print('## INIT a0 covar', pdf_a0.covariance_matrix[0,:,:]) # first sample
+
+def train_full(net, b_size, h_dim, x_dim,
+               T, checkpoint, direxp,
+               prop, obs, sigma0,
+               optimizer_classname, optimizer_kwargs):
+    
+    """
+    Train over full time 0..T with BPTT
+    # learn the parameters in net.a, net.b, net.c using t=0..T
+    # by minimizing the total loss
+    """
+    if not path.exists(direxp):
+        mkdir(direxp)          
+    
+    print('Train over full time 0..T with BPTT')
+
+    # generate training data seq for t=0..T
+    # TODO rewrite xt and yt
+    x0 = get_x0(b_size, x_dim, sigma0)
+    xt = [None]
+    yt = [None]
+    
+    # Train net using xt and yt, t = 1 .. T and x0
+    # TODO miminize total loss, by constructing optimizer and rewriting closure    
+    ite = 0
+    def closure():        
+        # TODO
+        
+        # Checkpoint
+        nonlocal ite
+        if ite == 1 or (ite % checkpoint == 0):
+            print("## Train Ite " + str(ite)+" ##")
+            save_dict(direxp,scores=net.scores)
+            print_scores(net.scores)
+        
+        return loss_total
+    
+    # TODO run optimizer
+    
+def train_online(net, b_size, h_dim, x_dim,
+                 T, checkpoint, direxp,
+                 prop, obs, sigma0,
+                 optimizer_classname, optimizer_kwargs, 
+                 scheduler_classname, scheduler_kwargs):
+    """
+    Train functions for the DAN, online and truckated BPTT
+    """
+    if not path.exists(direxp):
+        mkdir(direxp)
+        
+    # TODO construct optimizer and scheduler
+    assert(optimizer_classname != "NONE")
+    print(' optimizer_classname', optimizer_classname)
+
+    assert(scheduler_classname != "NONE")
+    print(' scheduler_classname', scheduler_classname)
+    
+    x0 = get_x0(b_size, x_dim, sigma0)
+    ha = get_ha0(b_size, h_dim)
+
+    for t in range(1, T+1):
+        # TODO
+        # on the fly data generation
+        # Truncated back propagation through time
+        ha = None
+        
+        # Checkpoint
+        if (t % checkpoint == 0) or (t == T):
+            if ha is not None:
+                print("## Train Cycle " + str(t)+" ##")
+                save_dict(direxp,
+                          net=net.state_dict(),
+                          ha=ha,
+                          x=x,
+                          scores=net.scores,
+                          optimizer=optimizer.state_dict())
+                print_scores(net.scores)
+
+@torch.no_grad()
+def test(net, b_size, h_dim, x_dim,
+         T, checkpoint, direxp,
+         prop, obs, sigma0):
+    x = get_x0_test(b_size, x_dim, sigma0)
+    ha = get_ha0(b_size, h_dim)
+    for t in range(1, T+1):
+        # on the fly data generation
+        x = prop(x)\
+            .sample(sample_shape=torch.Size([1]))\
+            .squeeze(0)
+        y = obs(x)\
+            .sample(sample_shape=torch.Size([1]))\
+            .squeeze(0)
+
+        # Evaluates the loss
+        _, ha = net(ha, x, y)
+
+        # Checkpoint
+        if (t % checkpoint == 0) or (t == T):
+            print("## Test Cycle " + str(t)+" ##")
+            save_dict(direxp,
+                      test_scores=net.scores)
+            print_scores(net.scores)
+
+def experiment(tensor_type, seed,
+               net_classname, net_kwargs,
+               sigma0, prop_kwargs, obs_kwargs,
+               train_kwargs, test_kwargs,
+               optimizer_classname, optimizer_kwargs,
+               scheduler_classname, scheduler_kwargs,
+               directory, nameexp):
+
+    # CPU or GPU tensor
+    cuda = torch.cuda.is_available()
+    set_tensor_type(tensor_type,cuda)
+
+    # Reproducibility
+    torch.manual_seed(seed)
+
+    net = eval(net_classname)(**net_kwargs)
+    prop = filters.Constructor(**prop_kwargs)
+    obs = filters.Constructor(**obs_kwargs)
+    b_size = train_kwargs['b_size']
+    h_dim = train_kwargs['h_dim']
+    x_dim = train_kwargs['x_dim']
+    T = train_kwargs['T']
+    checkpoint = train_kwargs['checkpoint']
+    direxp = directory + nameexp
+    
+    if train_kwargs["mode"] == "full":
+        pre_train_full(net,b_size,h_dim,x_dim,sigma0,
+                       optimizer_classname,optimizer_kwargs)        
+        train_full(net, b_size, h_dim, x_dim,
+                   T, checkpoint, direxp,
+                   prop, obs, sigma0,
+                   optimizer_classname, optimizer_kwargs)
+    else:
+        train_online(net, b_size, h_dim, x_dim,
+                     T, checkpoint, direxp,
+                     prop, obs, sigma0,
+                     optimizer_classname, optimizer_kwargs, 
+                     scheduler_classname, scheduler_kwargs)
+    
+    # Clear scores
+    net.clear_scores()
+
+    # Testing
+    b_size = test_kwargs['b_size']
+    h_dim = test_kwargs['h_dim']
+    x_dim = test_kwargs['x_dim']
+    T = test_kwargs['T']
+    checkpoint = test_kwargs['checkpoint']
+    test(net, b_size, h_dim, x_dim,
+         T, checkpoint, direxp,
+         prop, obs, sigma0)    
+
+
+def save_dict(prefix, **kwargs):
+    """
+    saves the arg dict val with name "prefix + key + .pt"
+    """
+    for key, val in kwargs.items():
+        torch.save(val, prefix + key + ".pt")
+
+
+def print_scores(scores):
+    for key, val in scores.items():
+        if len(val) > 0:
+            print(key+"= "+str(val[-1]))
+
+
+def update(k_default, k_update):
+    """Update a default dict with another dict
+    """
+    for key, value in k_update.items():
+        if isinstance(value, dict):
+            k_default[key] = update(k_default[key], value)
+        else:
+            k_default[key] = value
+    return k_default
+
+
+def update_and_save(k_default, list_k_update, name_fun):
+    """update and save a default dict for each dict in list_k_update,
+    generates a name for the exp with name_fun: dict -> string
+    returns the exp names on stdout
+    """
+    out, directory = "", k_default["directory"]
+    for k_update in list_k_update:
+        nameexp = name_fun(k_update)
+        if not os.path.exists(nameexp):
+            os.mkdir(nameexp)
+        k_default["nameexp"] = nameexp + "/"
+        torch.save(update(k_default, k_update), nameexp + "/kwargs.pt")
+        out += directory + "," + nameexp
+
+    # return the dir and nameexp
+    sys.stdout.write(out)
+
+
+if __name__ == "__main__":
+    """
+    the next argument is the experiment name
+    - launch the exp
+    """
+    torch.autograd.set_detect_anomaly(True)
+    cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if cuda else "cpu")
+    experiment(**torch.load(sys.argv[1] + "/kwargs.pt",
+                            map_location=torch.device(device)))
